277a8a4fad64effdf3dbc1bd36f8e561d0eb1a7e
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-04-27 23:48:33 +0530

    add string file

---
+++ Basics_Day_02/String.js
@@ -0,0 +1,9 @@
+const name = "Shashank"
+const repoCount = 19
+
+// console.log(name + repoCount + "value");
+
+console.log(`Hello my name is ${name} & my repo count is ${repoCount}`);
+
+const gameName = new String('Shashankhc')
+
