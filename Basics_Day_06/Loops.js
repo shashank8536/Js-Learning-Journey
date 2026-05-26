aff61110e969ca4ff2cc46b198477adf9ff462d0
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-05-27 02:22:32 +0530

    Study about for of loop and it's working

---
+++ Basics_Day_06/Loops.js
@@ -0,0 +1,42 @@
+// for of
+
+// ["","",""]
+// [{},{},{}]
+
+const arr = [1,2,3,4,5]
+
+// for(const num of arr){
+//     console.log(num)
+// }
+
+const greetings = "Hello World!"
+// for(const greet of greetings){
+//     console.log(`Each char is ${greet}`)
+// }
+
+// Maps -> known for unike values and ordered
+const map = new Map()
+map.set('IN','India')
+map.set('USA','United States Of America')
+map.set('Fr','France')
+
+// console.log(map)
+
+// for (const [key,value] of map) {
+//     console.log(key, ':-',value)
+    
+// }
+// MAps are iteratable by for of loop , Object are not iteratable
+
+
+
+const myObject = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+// for(const[key,value] of myObject){
+//     console.log(key,':-', value);
+// }
+
+                     //give error obj are not iteratable
