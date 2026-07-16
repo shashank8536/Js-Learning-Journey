6ec722ec629d878b9d5c3292514c8a7af54b2429
Shashank shekhar <shashankshekhargiri2003@gmail.com>
2026-07-16 23:53:04 +0530

    Add projects

---
+++ Projects_Day_09/Projects1.js
@@ -0,0 +1,51 @@
+// Project1 
+console.log("hitesh")
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (button) {
+    console.log(button);
+    button.addEventListener('click', function (e) {
+        console.log(e);
+        console.log(e.target);
+        if (e.target.id === 'grey') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'white') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'blue') {
+            body.style.backgroundColor = e.target.id;
+        }
+        if (e.target.id === 'yellow') {
+            body.style.backgroundColor = e.target.id;
+        }
+
+    });
+});
+
+// project 2
+
+
+const form = document.querySelector('form');
+// this usecase will give you empty
+// const height = parseInt(document.querySelector('#height').value)
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+
+  const height = parseInt(document.querySelector('#height').value);
+  const weight = parseInt(document.querySelector('#weight').value);
+  const results = document.querySelector('#results');
+
+  if (height === '' || height < 0 || isNaN(height)) {
+    results.innerHTML = `Please give a valid height ${height}`;
+  } else if (weight === '' || weight < 0 || isNaN(weight)) {
+    results.innerHTML = `Please give a valid weight ${weight}`;
+  } else {
+    const bmi = (weight / ((height * height) / 10000)).toFixed(2);
+    //show the result
+    results.innerHTML = `<span>${bmi}</span>`;
+  }
+});
+
